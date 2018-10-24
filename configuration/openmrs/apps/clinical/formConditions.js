403a87f5fab2a388c69bb576c3cb1ad96f270841
chine zoheir <chine.zoheir@gmail.com>
2018-10-24 18:57:16 +0800

    JIB-7: Display forms in dashboard and observation tab (#11)

---
+++ configuration/openmrs/apps/clinical/formConditions.js
@@ -0,0 +1,293 @@
+Bahmni.ConceptSet.FormConditions.rules = {
+    'JIB_Past Medical History' : function (formName, formFieldValues) {
+        var fieldValue = formFieldValues['JIB_Past Medical History'];
+
+        var sections = {
+            "show": [],
+            "hide": []
+        };
+
+        if (fieldValue && fieldValue.indexOf('JIB_Cancer') > -1) {
+            sections.show.push("JIB_Past Cancer Details");
+        } else {
+            sections.hide.push("JIB_Past Cancer Details");
+        }
+
+        return sections;
+    },
+    'JIB_BCG' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_BCG'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_BCG, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_BCG, Date Given"]
+            };
+        }
+    },
+    'JIB_HepB at birth' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_HepB at birth'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_HepB at birth, time administrered after birth"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_HepB at birth, time administrered after birth"]
+            };
+        }
+    },
+    'JIB_MR***' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_MR***'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_MR***, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_MR***, Date Given"]
+            };
+        }
+    },
+    'JIB_Polio1' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_Polio1'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_Polio1, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_Polio1, Date Given"]
+            };
+        }
+    },
+    'JIB_Polio2' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_Polio2'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_Polio2, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_Polio2, Date Given"]
+            };
+        }
+    },
+    'JIB_Polio3' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_Polio3'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_Polio3, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_Polio3, Date Given"]
+            };
+        }
+    },
+    'JIB_IPV' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_IPV'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_IPV, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_IPV, Date Given"]
+            };
+        }
+    },
+    'JIB_DTP-Hep-Hip1' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_DTP-Hep-Hip1'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_DTP-Hep-Hip1, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_DTP-Hep-Hip1, Date Given"]
+            };
+        }
+    },'JIB_DTP-Hep-Hip2' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_DTP-Hep-Hip2'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_DTP-Hep-Hip2, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_DTP-Hep-Hip2, Date Given"]
+            };
+        }
+    },
+    'JIB_DTP-Hep-Hip3' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_DTP-Hep-Hip3'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_DTP-Hep-Hip3, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_DTP-Hep-Hip3, Date Given"]
+            };
+        }
+    },
+    'JIB_PCV1' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_PCV1'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_PCV1, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_PCV1, Date Given"]
+            };
+        }
+    },
+    'JIB_PCV2' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_PCV2'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_PCV2, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_PCV2, Date Given"]
+            };
+        }
+    },
+    'JIB_PCV3' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_PCV3'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_PCV3, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_PCV3, Date Given"]
+            };
+        }
+    },
+    'JIB_MR1' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_MR1'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_MR1, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_MR1, Date Given"]
+            };
+        }
+    },
+    'JIB_JE' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_JE'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_JE, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_JE, Date Given"]
+            };
+        }
+    },
+    'JIB_Tetanus1' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_Tetanus1'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_Tetanus1, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_Tetanus1, Date Given"]
+            };
+        }
+    },
+    'JIB_Tetanus2' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_Tetanus2'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_Tetanus2, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_Tetanus2, Date Given"]
+            };
+        }
+    },
+    'JIB_Tetanus3' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_Tetanus3'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_Tetanus3, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_Tetanus3, Date Given"]
+            };
+        }
+    },
+    'JIB_Tetanus4' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_Tetanus4'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_Tetanus4, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_Tetanus4, Date Given"]
+            };
+        }
+    },
+    'JIB_Tetanus5' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_Tetanus5'];
+        if (fieldValue) {
+            return {
+                show: ["JIB_Tetanus5, Date Given"]
+            };
+        }
+        else {
+            return {
+                hide: ["JIB_Tetanus5, Date Given"]
+            };
+        }
+    },
+    'JIB_Genitourinary Female Symptoms' : function (formName, formFieldValues, patient) {
+        var fieldValue = formFieldValues['JIB_Genitourinary Female Symptoms'];
+        var patientGender = patient['gender'];
+        var sections = {
+            "show": [],
+            "hide": []
+        };
+        if (patientGender == 'M') {
+            sections.hide.push("JIB_Genitourinary Female Symptoms");
+            sections.hide.push("JIB_Genitourinary Female Exam Abnormalities");
+        } else if (patientGender == 'F') {
+            sections.hide.push("JIB_Genitourinary Male Symptoms");
+            sections.hide.push("JIB_Genitourinary Male Exam Abnormalities");
+        }
+        return sections;
+    }
+};
--- configuration/openmrs/apps/customDisplayControl/js/customControl.js
+++ configuration/openmrs/apps/customDisplayControl/js/customControl.js
@@ -1,3 +1,130 @@
 'use strict';
 
-angular.module('bahmni.common.displaycontrol.custom');
+angular.module('bahmni.common.displaycontrol.custom')
+.directive('immunization', ['observationsService', 'appService', 'spinner','$q', function (observationsService, appService, spinner, $q) {
+    var link = function ($scope) {
+
+        $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/immunization.html";
+
+        $scope.immunizations = [];
+
+        
+        // If the configration parameter is not present, return an promise that resolves an empty array
+        var fetchImmunizationSets = {};
+        if ($scope.config.immunizationSets == undefined) {
+            var deferred = $q.defer()
+            deferred.resolve([])
+            fetchImmunizationSets = deferred.promise;
+        } else {
+            fetchImmunizationSets = spinner.forPromise(observationsService.fetch($scope.patient.uuid, $scope.config.immunizationSets, "latest", undefined, undefined, undefined))
+        }
+
+        fetchImmunizationSets.then(function (response) {
+            var immunizationSets = response.data;
+            immunizationSets = _.map(immunizationSets, function(item, index) {
+                var immunization = {}
+                immunization.conceptUuid = item.groupMembers[0].conceptUuid;
+                immunization.name = item.groupMembers[0].conceptNameToDisplay;
+                immunization.value = item.groupMembers[0].valueAsString;
+                immunization.fullySpecifiedName = item.groupMembers[0].concept.name;
+
+                if (item.groupMembers.length > 1) {
+                    immunization.date = item.groupMembers[1].value;
+                }
+
+                return immunization
+            });
+
+            // If the configration parameter is not present, return an promise that resolves an empty array
+            var fetchImmunizationQuestions = {};    
+            if ($scope.config.immunizationQuestions == undefined) {
+                var deferred = $q.defer()
+                deferred.resolve([])
+                fetchImmunizationQuestions = deferred.promise;
+            } else {
+                fetchImmunizationQuestions = spinner.forPromise(observationsService.fetch($scope.patient.uuid, $scope.config.immunizationQuestions, undefined, undefined, undefined))
+            }
+            fetchImmunizationQuestions.then(function (response) {
+                var immunzationQuestions = response.data;
+
+                immunzationQuestions = _.map(immunzationQuestions, function (item, index) {
+                    var immunization =  {};
+                    immunization.conceptUuid = item.value.uuid;
+                    immunization.name = item.valueAsString;
+                    immunization.value = "OBS_BOOLEAN_YES_KEY";
+                    immunization.date = item.observationDateTime;
+                    immunization.fullySpecifiedName = item.value.name;
+
+                    return immunization
+                });
+
+                // Remove possible duplicates in the immunizationsQuestion to keep only the most recent ones.
+                var k = 0
+                var duplicatesToRemove = [];
+
+                for (k = immunzationQuestions.length - 1; k >= 0; k -= 1) {
+                    var itemK = immunzationQuestions[k]
+                    var l = 0;
+                    for (l = immunzationQuestions.length - 1; l >= 0; l -= 1) {
+                        var itemL = immunzationQuestions[l]
+                        if (itemK.conceptUuid == itemL.conceptUuid) {
+                            if (new Date(itemK.date) > new Date(itemL.date) ) {
+                                immunzationQuestions.splice(l)
+                            }
+                        }
+                    }
+                }
+
+                // Remove duplicated items between both immunizations list (keep only the most recent one)
+                var immunizationQuestionsToRemove = []
+                var immunizationSetsToRemove = []
+
+                var i;
+                for (i = immunizationSets.length - 1; i >= 0; i -= 1) {
+
+                    var item1 = immunizationSets[i]
+
+                    var j;
+                    for (j = immunzationQuestions.length - 1; j >= 0; j -= 1) {
+
+                        var item2 = immunzationQuestions[j];
+
+                        if (item1.conceptUuid == item2.conceptUuid) {
+                            if (new Date(item1.date) > new Date(item2.date) ) {
+                                immunizationQuestionsToRemove.push(j);
+                            } else {
+                                immunizationSetsToRemove.push(i);
+                            }
+
+                        }
+                    }
+                }
+
+                immunizationSetsToRemove.forEach(function (value) {
+                    immunizationSets.splice(value, 1);
+                })
+
+                immunizationQuestionsToRemove.forEach(function (value) {
+                    immunzationQuestions.splice(value, 1);
+                })
+
+                var allImmunizations = immunizationSets.concat(immunzationQuestions);
+
+                // Order the immunizations based on the config
+                $scope.config.immunizationsDisplayed.forEach(function(item1) {
+                    allImmunizations.forEach(function (item2) {
+                        if (item1 == item2.fullySpecifiedName) {
+                            $scope.immunizations.push(item2);
+                        }
+                    })
+                })
+            });
+        });
+
+    };
+        return {
+        restrict: 'E',
+        link: link,
+        template: '<ng-include src="contentUrl"/>'
+    }
+}]);
